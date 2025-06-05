96ebf836886d1a7266bab21a688b8d0e72223ebc
putra625 <putrastore128@gmail.com>
2025-06-05 20:16:24 +0700

    Update teksjpm.js

--- database/teksjpm.js
+++ database/teksjpm.js
@@ -1,3 +1,91 @@
-XlesyVIP
-KALO MAU BELI SCRIPT NO ENC 
-  PM https://wa.me/6283877636168
+*All Trx Open ✅*
+
+ | *Putz store Menyediakan* 🛍 : 
+- SC Bot wa
+- sewa bot grub / sewa bot pushkontak
+- MC & JP
+- Jasteb&Unchek
+- Jual/beli akun
+- nokos
+- suntik all sosmed
+- Murkos
+- Mursun
+- app buat anak JB
+- bug wa&unbug wa
+- own,PT,dll
+
+| *LIST SUNTIK ALL SOSMED BY PUTZ STORE*📍
+*SUNTIK FOLLOWERS TIKTOK⚡ :*
+- _*100 = 4k*_
+- _*200 = 7k*_ 
+- _*500 = 15k*_
+- _*700 = 20k*_
+- _*1000 = 28k*_ 
+- _*DAN HARGA SETERUSNYA*_
+
+*SUNTIK LIKE VIDEO TIKTOK⚡*
+* _*100=1k*_
+* _*500=3k*_
+* _*700=5k*_
+* _*1000=7k*_
+* _*1200=9k*_
+* _*DAN HARGA SETERUSNYA*_
+
+*SUNTIK VIEW TIKTOK⚡ :*
+* _*10.000 = 1,5k*_
+* _*20.000 = 2,5k*_
+* _*30.000 = 3,5k*_
+* _*40.000 = 4,5k*_
+* _*50.000 = 5,5k*_
+* _*DAN HARGA SETERUSNYA*_
+
+*SUNTIK FOLLOWERS INSTAGRAM⚡:*
+- _*100 = 4k*_ 
+* _*200 = 7k*_ 
+* _*500 = 15k*_ 
+* _*700 = 20k*_ 
+* _*1000 = 28k*_ 
+- _*DAN HARGA SETERUSNYA.*_
+
+*SUNTIK LIKE VIDEO INSTAGRAM⚡:*
+- _*100=1k*_
+- _*500 = 3k*_
+- _*1000 = 6k*_
+- _*2000 = 12k*_
+- _*5000 = 20k*_
+- _*DAN HARGA SETERUSNYA.*_
+
+*SUNTIK VIEW INSTAGRAM⚡ :*
+* _*10.000 = 1,5k*_
+* _*20.000 = 2,5k*_
+* _*30.000 = 3,5k*_
+* _*40.000 = 4,5k*_
+* _*50.000 = 5,5K*_
+- _*DAN HARGA SETERUSNYA.*_
+| *LIST SUNTIK SOSMED YANG LAIN PM ADMIN*
+
+*Menyediakan Juga*
+- _Apk kebutuhan anak JB_
+- _Sc Bug_
+- _Apk qris tanpa ktp_
+- _Nokos_
+- _spinan berhadiah_
+- _dll tanyakan saja_
+*🔥 Keuntungan Join Mursun🔥*
+*Mursun:5k*
+- _bisa suntik all sosmed sepuasnya_
+- _bisa jual jasa suntik sendiri_
+- _bisa open Mursun sendiri_
+- _mursun permanen murah_
+*🔥KEUNTUNGAN BUY Murkos🔥*
+*Murkos :10k*
+- _bisa create banyak nokos_
+- _jadi kang nokos_
+- _Bisa jualan nokos sendiri_
+- _nokos Siap Pakai_
+
+*# Mau Tanya-tanya?Pm*
+*[ Minat?Contact Putz Store ]*
+https://wa.me/6285800254519?text=*MIN+MAU+BELI+NIH*
+Channel Testimoni : whatsapp.com/channel/0029Vb6ayRFHbFUypgHV662K
+Marketplace : chat.whatsapp.com/C84ODpNupTHAxBOYIXq75W
